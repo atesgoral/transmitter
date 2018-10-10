c6169c4dcd8272d127dd889f214f90b3bfd7243b
Ates Goral <ates.goral@shopify.com>
2018-10-09 22:28:02 -0400

    Working (but tight) polar spiral iterator

--- iterators.js
+++ iterators.js
@@ -23,13 +23,26 @@ class ScanlineIterator {
 // @todo https://gamedev.stackexchange.com/questions/16745/moving-a-particle-around-an-archimedean-spiral-at-a-constant-speed
 class PolarSpiralIterator {
   constructor(w, h) {
-    const radius = Math.max(w, h) / 2;
+    const maxR = Math.max(w, h) / 2;
+    const v = Math.SQRT1_2;
+    const k = 1 / Math.PI / 2;
 
-    // @todo write dis codes
+    let t = 0;
 
     this.next = () => {
-      // @todo I can has scripts
-      return { x: Math.random() * w | 0, y: Math.random() * h | 0 };
+      let θ = Math.sqrt(t * v / k);
+      let r = θ * k;
+
+      if (r >= maxR) {
+        t = 0;
+      } else {
+        t += 1;
+      }
+
+      return {
+        x: Math.floor(Math.cos(θ) * r + w / 2),
+        y: Math.floor(Math.sin(θ) * r + h / 2)
+      };
     }
   }
 }
