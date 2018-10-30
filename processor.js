343b06b7060bfe58726bb91390801bbc735f3612
Ates Goral <ates.goral@shopify.com>
2018-10-29 20:57:27 -0400

    Broken audio worker

---
+++ processor.js
@@ -0,0 +1,31 @@
+class BufferConsumer extends AudioWorkletProcessor {
+  constructor(options) {
+    super(options);
+
+    this.buffers = [];
+
+    this.port.onmessage = (message) => {
+      this.buffers.push(message.data[0]);
+    }
+
+    // this.t = 0;
+  }
+
+  process(inputs, outputs) {
+    if (this.buffers.length) {
+      const buffer = this.buffers.shift();
+      // @todo force to be mono with options?
+      outputs[0][0].set(buffer);
+      this.port.postMessage('consumed');
+    }
+
+    // for (let i = 0; i < outputs[0][0]; i++) {
+    //   outputs[0][0][i] = Math.sin(t);
+    //   t += 0.01;
+    // }
+
+    return true;
+  }
+}
+
+registerProcessor('buffer-consumer', BufferConsumer);
