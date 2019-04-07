6e9668a339d9a410115b2b25023c56ac8be98073
Ates Goral <ates.goral@shopify.com>
2019-04-06 21:33:28 -0400

    Stashing struggles with AudioWorkletProcessor

--- processor.js
+++ processor.js
@@ -4,11 +4,11 @@ class BufferConsumer extends AudioWorkletProcessor {
 
     this.buffers = [];
 
-    this.port.onmessage = (message) => {
-      this.buffers.push(message.data[0]);
-    }
+    // this.port.onmessage = (message) => {
+    //   this.buffers.push(message.data[0]);
+    // }
 
-    // this.t = 0;
+    this.t = 0;
   }
 
   process(inputs, outputs) {
@@ -17,12 +17,15 @@ class BufferConsumer extends AudioWorkletProcessor {
       // @todo force to be mono with options?
       outputs[0][0].set(buffer);
       this.port.postMessage('consumed');
+      // console.log(this.buffers.length);
+    } else {
+      // console.log('no buffers');
     }
 
-    // for (let i = 0; i < outputs[0][0]; i++) {
-    //   outputs[0][0][i] = Math.sin(t);
-    //   t += 0.01;
-    // }
+    for (let i = 0; i < outputs[0][0].length; i++) {
+      outputs[0][0][i] = Math.sin(t);
+      t += 0.01;
+    }
 
     return true;
   }
